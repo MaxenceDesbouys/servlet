09f4b9d0eaa75bf41406f1f06565118b30e2834a
Maxence DESBOUYS <madesbouys@IUTCLINFA2319.iut.local>
2022-02-08 13:33:54 +0100

    omega

--- target/generated-sources/annotations/LigneTrain_.java
+++ target/generated-sources/annotations/LigneTrain_.java
@@ -3,7 +3,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-02T10:19:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-02T10:50:43")
 @StaticMetamodel(LigneTrain.class)
 public class LigneTrain_ { 
 
--- target/generated-sources/annotations/StationTrain_.java
+++ target/generated-sources/annotations/StationTrain_.java
@@ -2,7 +2,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-02T10:19:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-02-02T10:50:43")
 @StaticMetamodel(StationTrain.class)
 public class StationTrain_ { 
 
